865ef9c6f84dfedde6470831652ffbdc073f445d
George-Hachem <78202573+George-H12@users.noreply.github.com>
2023-05-18 15:35:49 -0400

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by George on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
